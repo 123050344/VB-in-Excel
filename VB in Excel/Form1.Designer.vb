6c50ea542870dcde8229466c0bf490d9a57557ba
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-05-01 12:15:19 -0600

    Agregar archivos de proyecto.

---
+++ VB in Excel/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lstvData = New ListView()
+        btnsave = New Button()
+        txtDescription = New TextBox()
+        lblCount = New Label()
+        lstItems = New ListView()
+        LblTotal = New Label()
+        btnRemove = New Button()
+        btnExportToExcel = New Button()
+        SuspendLayout()
+        ' 
+        ' lstvData
+        ' 
+        lstvData.Location = New Point(419, 158)
+        lstvData.Name = "lstvData"
+        lstvData.Size = New Size(369, 206)
+        lstvData.TabIndex = 0
+        lstvData.UseCompatibleStateImageBehavior = False
+        lstvData.View = View.List
+        ' 
+        ' btnsave
+        ' 
+        btnsave.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btnsave.Location = New Point(419, 12)
+        btnsave.Name = "btnsave"
+        btnsave.Size = New Size(167, 47)
+        btnsave.TabIndex = 1
+        btnsave.Text = "Save"
+        btnsave.UseVisualStyleBackColor = True
+        ' 
+        ' txtDescription
+        ' 
+        txtDescription.Location = New Point(36, 39)
+        txtDescription.Multiline = True
+        txtDescription.Name = "txtDescription"
+        txtDescription.Size = New Size(333, 60)
+        txtDescription.TabIndex = 2
+        ' 
+        ' lblCount
+        ' 
+        lblCount.AutoSize = True
+        lblCount.Location = New Point(433, 394)
+        lblCount.Name = "lblCount"
+        lblCount.Size = New Size(0, 15)
+        lblCount.TabIndex = 3
+        ' 
+        ' lstItems
+        ' 
+        lstItems.Location = New Point(21, 158)
+        lstItems.Name = "lstItems"
+        lstItems.Size = New Size(369, 206)
+        lstItems.TabIndex = 4
+        lstItems.UseCompatibleStateImageBehavior = False
+        lstItems.View = View.List
+        ' 
+        ' LblTotal
+        ' 
+        LblTotal.AutoSize = True
+        LblTotal.Location = New Point(93, 379)
+        LblTotal.Name = "LblTotal"
+        LblTotal.Size = New Size(0, 15)
+        LblTotal.TabIndex = 5
+        ' 
+        ' btnRemove
+        ' 
+        btnRemove.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btnRemove.Location = New Point(419, 82)
+        btnRemove.Name = "btnRemove"
+        btnRemove.Size = New Size(167, 47)
+        btnRemove.TabIndex = 6
+        btnRemove.Text = "Remove"
+        btnRemove.UseVisualStyleBackColor = True
+        ' 
+        ' btnExportToExcel
+        ' 
+        btnExportToExcel.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btnExportToExcel.Location = New Point(612, 52)
+        btnExportToExcel.Name = "btnExportToExcel"
+        btnExportToExcel.Size = New Size(167, 47)
+        btnExportToExcel.TabIndex = 7
+        btnExportToExcel.Text = "Export"
+        btnExportToExcel.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnExportToExcel)
+        Controls.Add(btnRemove)
+        Controls.Add(LblTotal)
+        Controls.Add(lstItems)
+        Controls.Add(lblCount)
+        Controls.Add(txtDescription)
+        Controls.Add(btnsave)
+        Controls.Add(lstvData)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lstvData As ListView
+    Friend WithEvents btnsave As Button
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents lblCount As Label
+    Friend WithEvents lstItems As ListView
+    Friend WithEvents LblTotal As Label
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnExportToExcel As Button
+End Class
---
+++ VB in Excel/Form1.vb
@@ -0,0 +1,89 @@
+﻿
+Imports System.IO
+Imports OfficeOpenXml
+
+Public Class Form1
+
+    Dim id As Integer
+
+    Public Sub New()
+        InitializeComponent()
+        id = 0
+        lstvData.FullRowSelect = True
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
+        Dim x As MyItem
+
+        id = id + 1
+        Dim description As String = txtDescription.Text
+        Dim price As Decimal = Math.Round((New Random()).NextDouble() * 1000, 2)
+        x = New MyItem(id, description, price)
+        lstItems.Items.Add(x.ToString())
+
+        Dim row As New ListViewItem({x.Id.ToString(), x.Description, x.Price.ToString()})
+        lstvData.Items.Add(row)
+
+        UpdateLabel()
+        UpdateTotal()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        For Each item As ListViewItem In lstvData.SelectedItems
+            lstvData.Items.Remove(item)
+        Next
+        UpdateLabel()
+        UpdateTotal()
+    End Sub
+
+    Sub UpdateLabel()
+        lblCount.Text = lstvData.Items.Count.ToString()
+    End Sub
+
+    Sub UpdateTotal()
+        Dim total As Decimal = 0
+        For Each item As ListViewItem In lstvData.Items
+            total += Decimal.Parse(item.SubItems(2).Text)
+        Next
+        LblTotal.Text = total.ToString()
+    End Sub
+
+    Private Sub ExportToExcel_Click(sender As Object, e As EventArgs) Handles btnExportToExcel.Click
+
+        If lstvData.Items.Count = 0 Then
+            MessageBox.Show("There is no data to export.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+
+        Dim desktopPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+        Dim filePath As String = Path.Combine(desktopPath, "archive.xlsx")
+
+        Try
+
+            Using package As New ExcelPackage(New FileInfo(filePath))
+                Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets.Add("Datas")
+                worksheet.Cells("A1").Value = "ID"
+                worksheet.Cells("B1").Value = "Descriptión"
+                worksheet.Cells("C1").Value = "Price"
+
+                Dim row As Integer = 2
+                For Each item As ListViewItem In lstvData.Items
+                    worksheet.Cells("A" & row).Value = item.SubItems(0).Text
+                    worksheet.Cells("B" & row).Value = item.SubItems(1).Text
+                    worksheet.Cells("C" & row).Value = item.SubItems(2).Text
+                    row += 1
+                Next
+
+
+                package.Save()
+            End Using
+
+            MessageBox.Show("Data exported to Excel successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Error when exporting data to Excel: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+End Class
+
